b9aea6fee26ec0811b6fa013b1e8d9c00dbdb5d3
Andre Figueiredo <159852958+Quasi-Liminality@users.noreply.github.com>
2025-02-21 18:23:30 -0300

    Implement extra credit features

--- sketch.js
+++ sketch.js
@@ -1,28 +1,35 @@
-let container = document.querySelector('.container');
-
-let gridDimensions = 16;
-createGrid(gridDimensions);
+let grid = document.querySelector('.grid');
+let initialGridDimensions = 16;
+let randomNum = () => Math.floor(Math.random() * 256);
 
 function createGrid(dimensions) {
-    container.innerHTML = '';
-    container.style.setProperty('--container-children', dimensions);
+    newGrid = [];
     for (let i = 0; i < dimensions ** 2; i++) {
         let div = document.createElement('div');
-        container.appendChild(div);
+        newGrid.push(div);
     }
+    grid.style.setProperty('--grid-children', dimensions);
+    grid.replaceChildren(...newGrid);
 }
 
-document.querySelector('#resizeBtn').addEventListener('click', () => {
+createGrid(initialGridDimensions);
+
+function resizeGrid() {
     let newDimensions = (() => {
         let dimensions;
         do {
             dimensions = prompt('How many rows/columns should the grid have (1-100)?');
+            if (dimensions === null) return false; // User cancelled the prompt
         } while (!parseInt(dimensions) || dimensions < 1 || dimensions > 100);
         return dimensions;
     })();
+    if (!newDimensions) return;
     createGrid(newDimensions);
-});
+}
 
-container.addEventListener('mousemove', (e) => {
-    e.target.style.backgroundColor = 'red';
+document.querySelector('#resizeBtn').addEventListener('click', resizeGrid);
+
+grid.addEventListener('mouseover', (e) => {
+    e.target.style.backgroundColor = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+    e.target.style.opacity = +e.target.style.opacity + 0.1;
 });
